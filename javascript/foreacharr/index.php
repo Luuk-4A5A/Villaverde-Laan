99be518e36e43c1b1659702d8b6e5aabf6a93a71
unknown <501287@edu.rocmn.nl>
2017-09-22 10:57:49 +0200

    updated foreach assignment

--- javascript/foreacharr/index.php
+++ javascript/foreacharr/index.php
@@ -4,6 +4,15 @@
 	<title></title>
 </head>
 <body>
+	<ul data-show-oddeven>
+		<li>A</li>
+		<li>B</li>
+		<li>C</li>
+		<li>D</li>
+		<li>E</li>
+		<li>F</li>
+	</ul>
+
 <script src="main.js"></script>
 </body>
 </html>
--- javascript/foreacharr/main.js
+++ javascript/foreacharr/main.js
@@ -4,3 +4,19 @@ numbers.forEach((value, index, arr) => {
 	var val = (typeof arr[index + 1] === 'undefined') ? arr[index] : arr[index] + arr[index + 1];
 	console.log('item ' + index + ' = ' + val);
 });
+
+
+var oddEven = document.querySelectorAll('ul[data-show-oddeven] li');
+
+oddEven.forEach((value, index, arr) => {
+	if(calcOddEven(index + 1)) {
+		value.title = 'even';
+	} else {
+		value.title = 'odd';
+	}
+});
+
+
+function calcOddEven(number) {
+	return number % 2;
+}
